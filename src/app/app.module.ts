c98a0a5eb1724bdb9b888ae2e289adf92d869597
Andres Solano S <andres.solano@gmail.com>
2019-01-11 13:54:52 -0600

    align title, fixes and imrovements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
 import { AmbNavComponent } from './shared/components/amb-nav/amb-nav.component';
-import { AmbFooterComponent } from './amb-footer/amb-footer.component';
+import { AmbFooterComponent } from './shared/components/amb-footer/amb-footer.component';
 import { StoreModule } from '@ngrx/store';
 import { reducers } from './core/appState';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from 'src/environments/environment';
 import { MaterialModule } from './shared/material/material.module';
 import { AppRoutingModule } from './app.routing.module';
+
 
 @NgModule({
   declarations: [
