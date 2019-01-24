f9888f8e578d17e8230b7bd282b7e1311bdbe9ff
Andres Solano S <andres.solano@gmail.com>
2019-01-24 14:11:08 -0600

    avoid home redirect

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,12 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
 
-  // {
-  //     path: '',
-  //     pathMatch: 'full',
-  //     loadChildren: './amb-home/amb-home.module#AmbHomeModule'
-  // },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+      path: '',
+      pathMatch: 'full',
+      loadChildren: './amb-home/amb-home.module#AmbHomeModule'
+  },
+  // { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: 'home',
     pathMatch: 'full',
